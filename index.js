9ad449a89f3d3467412fdff290fea3bc71518d56
Matthew Johnston <mattdjohnston92@gmail.com>
2022-11-17 14:05:27 -0400

    Add project files.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/note');
 
 const app = express();
 
@@ -9,28 +11,28 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
+// let persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ];
 
 app.get('/info', (request, response) => {
   let date = new Date().toString();
@@ -44,7 +46,9 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -83,25 +87,32 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  const existingPerson = persons.find(person => person.name === name);
-  if (existingPerson) {
-    return response.status(400).json({
-      error: 'name already exists'
-    });
-  }
+  // const existingPerson = persons.find(person => person.name === name);
+  // if (existingPerson) {
+  //   return response.status(400).json({
+  //     error: 'name already exists'
+  //   });
+  // }
 
-  const person = {
-    id: Math.ceil(Math.random() * 999999999999999999999),
+  // const person = {
+  //   id: Math.ceil(Math.random() * 999999999999999999999),
+  //   name: name,
+  //   number: number
+  // }
+
+  // persons = persons.concat(person);
+
+  const person = new Person({
     name: name,
     number: number
-  }
+  });
 
-  persons = persons.concat(person);
-  
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
